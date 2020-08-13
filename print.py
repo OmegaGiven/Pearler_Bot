142e0f3484c3514f278aa824ebde64e273835f5f
Nathan Johnson <nathan9j@gmail.com>
2020-08-13 15:50:15 -0600

    ajn

--- print.py
+++ print.py
@@ -1,12 +1,14 @@
 from xyMovement import move_x, move_y
 from colorSelector import move_pusher, move_rotator
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 def goto(startpointX, startpointY, startColor):
     move_rotator(startColor)
     move_pusher()
     move_x(startpointX)
     move_y(startpointY)
+
 
 
 def pearl(print_list):
@@ -17,4 +19,5 @@ def pearl(print_list):
 def thread_pearl(print_list):
     for point in print_list :
         goto(int(point[0]), int(point[1]), int(point[2]))
+    print("testing print")
 
