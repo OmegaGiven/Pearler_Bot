bf1b362257129a85c890388340b522b8f4941082
Nathan Johnson <nathan9j@gmail.com>
2020-08-13 14:14:37 -0600

    fixed syntx

--- print.py
+++ print.py
@@ -5,7 +5,7 @@ from colorSelector import move_pusher, move_rotator
 def goto(startpointX, startpointY, startColor):
     move_x(startpointX)
     move_y(startpointY)
-    move_rotator(move_rotator(startColor))
+    move_rotator(startColor)
     move_pusher()
 
 
