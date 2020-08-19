ac5c1ac640f55d2c83be74846bb8903b569bb488
OmegaGiven <nathan9j@gmail.com>
2020-08-18 22:23:12 -0600

    fixed syntax

--- startPrint.py
+++ startPrint.py
@@ -20,7 +20,7 @@ def pearl(print_list):
 
 def thread_pearl(print_list):
     for point in print_list :
-        if point[1]//2 == 1:
+        if float(point[1]//2) == 1:
             goto(int(point[0]), int(point[1]), int(point[2]))
         else:
             goto(int(-point[0]), int(point[1]), int(point[2]))
